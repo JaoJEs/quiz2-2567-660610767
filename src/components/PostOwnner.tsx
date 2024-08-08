783ce84035f740fcb59c632621009f57ff7b5c0d
Niphitpon Thosap <NiphitponT.63@cmu.ac.th>
2024-08-08 15:26:05 +0700

    addOwn

--- src/components/PostOwnner.tsx
+++ src/components/PostOwnner.tsx
@@ -1,24 +1,25 @@
 "use client";
 import React from "react";
+
 export default function PostOwnner({}) {
   return (
   <div className="vstack gap-3">
   <div className="d-flex align-items-center gap-3">
     <img
-      src="/profileImages/myself.jpg"
+      src={ImagePath}
       width="48"
       height="48"
       className="rounded-circle"
       style={{ objectFit: "cover" }}
     />
-    <span className="fw-semibold fs-5">NIPHITPON THOSAP 660610767</span>
+    <span className="fw-semibold fs-5">{fullName} {studentId}</span>
   </div>
 
-  <span>Quiz ง่ายจังเลยครับ ขอยาก ๆ กว่านี้ได้ไหม #261207</span>
+  <span>{postTitle}</span>
 
   <div className="d-flex align-items-center gap-1">
-    <img src="/like.svg" width={20}></img>
-    <span className="text-muted">100 คน</span>
+    <img src="/like.png" width={20}></img>
+    <span className="text-muted">{likeNum} คน</span>
   </div>
   <hr className="m-0 border" />
 </div>
