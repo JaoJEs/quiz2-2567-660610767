2dff6519a67cd175f28c5c3b8fc25caa2758682f
Niphitpon Thosap <NiphitponT.63@cmu.ac.th>
2024-08-08 15:22:40 +0700

    add

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,22 +1,18 @@
 "use client";
+import reply from " /src/app/libs/comments.js"
+
 export default function Comment({
   ImagePath,
-  username:string,
+  username,
   commentTitle,
-  likes:number,
+  likes,
   replies,
-}) {
+}): CommentProps {
   return (
     <div>
-      your code for Comment component here ...
-      {/* You can use map-loop to render Reply component here */}
-    </div>
-  );
-}
-
-<div className="d-flex gap-2 my-2">
+      <div className="d-flex gap-2 my-2">
           <img
-            src="/profileImages/lisa.jpg"
+            src= {ImagePath}
             width="48"
             height="48"
             className="rounded-circle"
@@ -26,12 +22,23 @@ export default function Comment({
             className="rounded rounded-3 p-2"
             style={{ backgroundColor: "#E5E7EB" }}
           >
-            <span className="fw-semibold">Lisa</span>
+            <span className="fw-semibold">{username}</span>
             <br />
-            <span>จริงค่า</span>
+            <span>{commentTitle}</span>
             <div className="d-flex align-items-center gap-1">
-              <img src="/profileImages/like.png" width={20}></img>
-              <span className="text-muted">999 คน</span>
+              <img src="/like.png" width={20}></img>
+              <span className="text-muted">{likes} คน</span>
             </div>
+            }
           </div>
         </div>
+        {replies.map(reply => {
+          return(
+            <Reply {...reply}key={reply?.username} />
+            
+          )
+        }
+    </div>
+  );
+}
+
